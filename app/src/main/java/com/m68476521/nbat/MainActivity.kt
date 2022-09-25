cc9808c5792cc477f742aad21fbedbf42088bea7
Mike Wasauskee <m68476521.1@gmail.com>
2022-09-24 22:26:43 -0700

    Add a legend

--- app/src/main/java/com/m68476521/nbat/MainActivity.kt
+++ app/src/main/java/com/m68476521/nbat/MainActivity.kt
@@ -2,7 +2,7 @@ package com.m68476521.nbat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// Develop by m68476521
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
