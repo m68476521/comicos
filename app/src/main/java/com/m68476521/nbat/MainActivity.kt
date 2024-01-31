073069dddd61e75a75a69a7c416b9bfbac11b50a
Mike Wasauskee <m68476521.1@gmail.com>
2024-01-31 12:04:07 -0800

    WIP final changes 24-01

--- app/src/main/java/com/m68476521/nbat/MainActivity.kt
+++ app/src/main/java/com/m68476521/nbat/MainActivity.kt
@@ -3,6 +3,8 @@ package com.m68476521.nbat
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
+//This is the final project where I should be working
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/m68476521/nbat/model/MyModel.kt
+++ app/src/main/java/com/m68476521/nbat/model/MyModel.kt
@@ -5,13 +5,14 @@ import androidx.lifecycle.viewModelScope
 import com.m68476521.nbat.repository.ApiRepository
 import com.m68476521.nbat.repository.ImageResponse
 import kotlinx.coroutines.launch
+import okhttp3.ResponseBody
 import javax.inject.Inject
 
 class MyModel : ViewModel() {
     @Inject
     lateinit var repository1: ApiRepository
 
-    val data = mutableListOf<ImageResponse>()
+    val data = mutableListOf<ResponseBody>()
 
     fun getData() {
         viewModelScope.launch { data.add(repository1.getSessions()) }
