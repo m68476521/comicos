e0c7311ec9bc22e6a5758f940faca6d4d60978d4
Miguel Orozco <m68476521.1@gmail.com>
2024-06-30 07:31:14 -0700

    Import Compose, Refactor fetch data
    In this commit, I update the import for compose. I also refactor the data class and the View model.
    I remove the xml from home fragment and I use a lazy column from compose and show the title of the data.
    
    Develop by m68476521

--- app/src/main/java/com/m68476521/comicos/home/HomeFragment.kt
+++ app/src/main/java/com/m68476521/comicos/home/HomeFragment.kt
@@ -4,31 +4,72 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.m68476521.comicos.databinding.FragmentHomeBinding
 import com.m68476521.comicos.model.MyModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
+
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    private val viewModel : MyModel by viewModels()
-    private lateinit var binding : FragmentHomeBinding
+    private val viewModel: MyModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        lifecycleScope.launch {
-            val response = viewModel.getData()
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MaterialTheme {
+                    myList(viewModel)
+                }
+            }
         }
     }
+
+    @Composable
+    fun myList(viewModel: MyModel) {
+        val data by viewModel.comicsResponseData.collectAsState()
+
+        LaunchedEffect(key1 = Unit) {
+            viewModel.getData()
+        }
+
+        val result = data.data?.results ?: return
+        LazyColumn {
+
+            items(result) { currentName ->
+                Text(
+                    text = currentName.title.toString(),
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+                HorizontalDivider(
+                    modifier = Modifier
+                        .padding(6.dp),
+                    thickness = 2.dp
+                )
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/m68476521/comicos/repository/Model.kt
+++ app/src/main/java/com/m68476521/comicos/repository/Model.kt
@@ -2,11 +2,117 @@ package com.m68476521.comicos.repository
 
 import com.google.gson.annotations.SerializedName
 
-data class ImageResponse(
-    val data: List<Image>,
-    val pagination: Pagination
+data class ComicsResponse(
+    val code: Int? = null,
+    val status: String? = null,
+    val data: DataResponse? = null
 )
 
+data class DataResponse(
+    val offset: Int? = null,
+    val limit: Int? = null,
+    val total: Int? = null,
+    val count: Int? = null,
+    val results: List<Results>? = null
+)
+
+data class Results(
+    val id: Int? = null,
+    val digitalId : Int? = null,
+    val title: String? = null,
+    val issueNumber: Int? = null,
+    val variantDescription:String? = null,
+    val description:String? = null,
+    val modified:String? = null,
+    val isbn:String? = null,
+    val upc:String? = null,
+    val diamondCode:String? = null,
+    val ean:String? = null,
+    val issn:String? = null,
+    val format:String? = null,
+    val pageCount: Int? = null,
+    //val textObjects:String? = null,
+    val resourceURI:String? = null,
+    val urls: List<UrlData>? = null,
+    val series: Serie? = null,
+    val variants: List<Variant>? = null,
+    val collections: List<Variant>? = null,
+    val collectedIssues: List<Variant>? = null,
+    val dates: List<DateData>? = null,
+    val prices: List<Price>? = null,
+    val thumbnail: Thumbnail? = null,
+    val images: List<Thumbnail>? = null,
+    val creators: Creator? = null,
+    val characters: Characters? = null,
+    val stories: Storie? = null,
+    val events: Event? = null
+)
+
+data class  UrlData(
+    val type: String? = null,
+    val url: String? = null
+)
+data class Serie(
+    val resourceURI: String? = null,
+    val name: String? = null
+)
+
+data class Variant(
+    val resourceURI: String? = null,
+    val name: String? = null
+)
+data class DateData(
+    val type: String? = null,
+    val date: String? = null
+)
+data class Price(
+    val type: String? = null,
+    val price: Double? = null
+)
+data class Thumbnail(
+    val path: String? = null,
+    val extension: String? = null
+)
+
+data class Creator(
+    val available: Int? = null,
+    val collectionURI: String? = null,
+    val items: List<ItemCreator>? = null,
+    val returned: Int? = null
+)
+
+data class ItemCreator(
+    val resourceURI: String? = null,
+    val name: String? = null,
+    val role: String? = null
+)
+
+data class Characters(
+    val available: Int? = null,
+    val collectionURI: String? = null,
+    val items: List<ItemCreator>? = null,
+    val returned: Int? = null
+)
+
+data class Storie(
+    val available: Int? = null,
+    val collectionURI: String? = null,
+    val items: List<ItemStorie>? = null,
+    val returned: Int? = null
+)
+
+data class ItemStorie(
+    val resourceURI: String? = null,
+    val name: String? = null,
+    val type: String? = null
+)
+
+data class Event(
+    val available: Int? = null,
+    val collectionURI: String? = null,
+    val items: List<ItemStorie>? = null,
+    val returned: Int? = null
+)
 data class Pagination(
     @SerializedName("total_count")
     val totalCount: Int,
