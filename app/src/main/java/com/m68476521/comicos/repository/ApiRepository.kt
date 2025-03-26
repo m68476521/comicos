5cda3b4c7d58c54d8faa1d69ce3a200162d69ea4
Miguel Orozco <m68476521.1@gmail.com>
2025-03-26 14:45:18 -0700

    Add library Arrow

--- app/src/main/java/com/m68476521/comicos/repository/ApiRepository.kt
+++ app/src/main/java/com/m68476521/comicos/repository/ApiRepository.kt
@@ -1,8 +1,11 @@
 package com.m68476521.comicos.repository
 
+import arrow.core.Either
 import javax.inject.Inject
 
 open class ApiRepository @Inject constructor(private val apiService: ApiService) {
     // TODO remove this hardCoded
-    open suspend fun getSessions() = apiService.getSeasons()
+    open suspend fun getSessions(): Either<Throwable, ComicsResponse> = Either.catch {
+        apiService.getSeasons()
+    }
 }
