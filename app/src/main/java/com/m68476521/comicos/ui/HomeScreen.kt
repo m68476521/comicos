a145aef59ed1e0243dca33f5c63791e139f62cfb
Miguel Orozco <m68476521.1@gmail.com>
2025-07-31 20:41:13 -0700

    FIX: Add tile at tile from HomeScreen
    Develop by Miguel Orozco <m68476521.1@gmail.com>

--- app/src/main/java/com/m68476521/comicos/ui/HomeScreen.kt
+++ app/src/main/java/com/m68476521/comicos/ui/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.m68476521.comicos.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,13 +12,22 @@ import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.m68476521.comicos.model.MyModel
 
@@ -40,7 +50,7 @@ fun HomeScreen(
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         content = {
             items(result) { currentItem ->
-                var image = currentItem.thumbnail?.path + "." + currentItem.thumbnail?.extension
+                val image = currentItem.thumbnail?.path + "." + currentItem.thumbnail?.extension
                 if (image.startsWith("http:")) {
                     image.replace("http:", "https:")
                 }
@@ -53,14 +63,38 @@ fun HomeScreen(
                         itemSelected.invoke(currentItem.title, currentItem.id.toString())
                     }
                 ) {
-                    AsyncImage(
-                        model = image,
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight()
-                    )
+
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        AsyncImage(
+                            model = image,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight()
+                        )
+
+                        Text(
+                            text = currentItem.title ?: "",
+                            style = TextStyle(
+                                color = MaterialTheme.colorScheme.onSurface,
+                                fontSize = 14.sp,
+                                shadow = Shadow(
+                                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                                    offset = Offset(5f, 5f),
+                                    blurRadius = 8f
+                                )
+                            ),
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(4.dp)
+                                .align(Alignment.BottomCenter)
+                        )
+                    }
                 }
             }
         },
